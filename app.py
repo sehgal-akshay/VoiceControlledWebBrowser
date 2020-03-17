ac84f5d4c142aaed3e0a10dcc2bb59a24dd289d3
sehgal-akshay <akshaysehgal19@gmail.com>
2020-03-16 21:47:40 -0400

    final draft version 1

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ from urllib.request import urlopen
 import wikipedia
 import random
 from time import strftime
+import certifi
 
 
 def openChrome():
@@ -44,6 +45,44 @@ def switchTabForward(direction, flag):
         browser.switch_to_window(browser.window_handles[-1])
     # print(browser.current_window_handle)
     # print(browser.window_handles.index(browser.current_window_handle))
+
+
+def browserNotOpen():
+    sofiaResponse(
+        'Please open the browser. Use command open browser to open chrome browser')
+
+
+def initalPrompt():
+    sofiaResponse('Welcome to browser Assistant. How can I help you?')
+
+
+def ErrorPrompt():
+    sofiaResponse('I didn’t understand. I can help you with opening and closing of websites, switch tabs, read news for you, tell you about weather, time and jokes, login into facebook and more. Please refer to the set of commands to perform tasks.')
+
+
+def scrollBrowser():
+    global browser
+    browser.execute_script("window.scrollBy(0,1000)")
+    # browserNotOpen()
+
+
+def search():
+    global browser
+    global tabFlag
+    openChrome()
+    if tabFlag:
+        browser.execute_script(
+            '''window.open("https://www.google.com","_blank");''')
+    else:
+        browser.get('https://www.google.com')
+        tabFlag = True
+    elem = browser.find_element_by_name('q')
+    elem.clear()
+    sofiaResponse('what you want me to search')
+    entity = myCommand()
+    elem.send_keys(entity)
+    button = browser.find_element_by_name('btnK')
+    button.submit()
 
 
 def openWebpage(name):
@@ -129,6 +168,41 @@ def newsRead():
             break
 
 
+def ErrorResponse():
+    sofiaResponse(
+        'I did not understand that. I can help you with time, opening and closing of browser and')
+
+
+def loginFacebook():
+    global browser
+    global tabFlag
+    if tabFlag:
+        browser.execute_script(
+            '''window.open("https://www.facebook.com","_blank");''')
+    else:
+        browser.get('https://www.facebook.com')
+        tabFlag = True
+    username = browser.find_element_by_id(
+        'email')
+    username.clear()
+    username.send_keys("shivamagarwal2996@gmail.com")
+    pwd = browser.find_element_by_id('pass')
+    pwd.clear()
+    pwd.send_keys("alpha123")
+    login = browser.find_element_by_id('u_0_2')
+    login.click()
+
+
+def close(value):
+    global browser
+    if value == 'browser':
+        browser.quit()
+    if value == 'window':
+        browser.switch_to_window(browser.window_handles[browser.window_handles.index(
+            browser.current_window_handle)])
+        browser.close()
+
+
 def assistant(command):
     global browser
     "if statements for executing commands"
@@ -165,20 +239,8 @@ def assistant(command):
         else:
             sofiaResponse('Hello Sir. Good evening')
     elif 'help me' in command:
-        sofiaResponse("""
-        You can use these commands and I'll help you out:
-        1. Open reddit subreddit : Opens the subreddit in default browser.
-        2. Open xyz.com : replace xyz with any website name
-        3. Send email/email : Follow up questions such as recipient name, content will be asked in order.
-        4. Current weather in {cityname} : Tells you the current condition and temperture
-        5. Hello
-        6. play me a video : Plays song in your VLC media player
-        7. change wallpaper : Change desktop wallpaper
-        8. news for today : reads top news of today
-        9. time : Current system time
-        10. top stories from google news (RSS feeds)
-        11. tell me about xyz : tells you about xyz
-        """)
+        sofiaResponse(
+            'You can use these commands and I will help you out: 1. Open reddit subreddit : Opens the subreddit in default browser. 2. Open xyz.com : replace xyz with any website name 3. Send email/email : Follow up questions such as recipient name, content will be asked in order. 4. Current weather in {cityname} : Tells you the current condition and temperture. 5. time : Current system time')
     # maximize and minimize windows
     elif 'maximize' in command:
         maximize()
@@ -193,21 +255,26 @@ def assistant(command):
             sofiaResponse(str(res.json()['joke']))
         else:
             sofiaResponse('oops!I ran out of jokes')
-    elif 'read me google news' in command:
+    elif 'read me news' in command:
         newsRead()
+    elif 'search' in command:
+        search()
     # top stories from google news
-    elif 'news for today' in command:
-        try:
-            news_url = "https://news.google.com/news/rss"
-            Client = urlopen(news_url)
-            xml_page = Client.read()
-            Client.close()
-            soup_page = soup(xml_page, "xml")
-            news_list = soup_page.findAll("item")
-            for news in news_list[:15]:
-                sofiaResponse(news.title.text.encode('utf-8'))
-        except Exception as e:
-            print(e)
+    # elif 'news for today' in command:
+    #     try:
+    #         news_url = "https://news.google.com/news/rss"
+    #         # Client = urlopen(news_url)
+    #         Client = urllib2.urlopen(news_url, cafile=certifi.where())
+    #         xml_page = Client.read()
+    #         Client.close()
+    #         soup_page = soup(xml_page, "lxml")
+    #         news_list = soup_page.findAll("item")
+    #         for news in news_list[:15]:
+    #             sofiaResponse(news.title.text.encode('utf-8'))
+    #     except Exception as e:
+    #         print(e)
+    elif 'login facebook' in command:
+        loginFacebook()
     # current weather
     elif 'current weather' in command:
         reg_ex = re.search('current weather in (.*)', command)
@@ -220,33 +287,23 @@ def assistant(command):
             x = w.get_temperature(unit='celsius')
             sofiaResponse('Current weather in %s is %s. The maximum temperature is %0.2f and the minimum temperature is %0.2f degree celcius' % (
                 city, k, x['temp_max'], x['temp_min']))
-    elif 'close browser' in command:
-        global browser
-        browser.quit()
+    elif 'scroll' in command:
+        scrollBrowser()
+    elif 'close' in command:
+        if 'window' in command:
+            close('window')
+        elif 'browser' in command:
+            close('browser')
+
         # time
     elif 'time' in command:
         import datetime
         now = datetime.datetime.now()
         sofiaResponse('Current time is %d hours %d minutes' %
                       (now.hour, now.minute))
-        # reg_ex = re.search('open reddit (.*)', command)
-        # url = 'https://www.reddit.com/'
-        # if reg_ex:
-        #     subreddit = reg_ex.group(1)
-        #     url = url + 'r/' + subreddit
-        # webbrowser.open(url)
-        # sofiaResponse('The Reddit content has been opened for you Sir.')
+
     # play youtube song
     elif 'play me a song' in command:
-        # path = '/Users/nageshsinghchauhan/Documents/videos/'
-        # folder = path
-        # for the_file in os.listdir(folder):
-        #     file_path = os.path.join(folder, the_file)
-        #     try:
-        #         if os.path.isfile(file_path):
-        #             os.unlink(file_path)
-        #     except Exception as e:
-        #         print(e)
         sofiaResponse('What song shall I play Sir?')
         mysong = myCommand()
         if mysong:
@@ -264,13 +321,17 @@ def assistant(command):
                     url_list.append(final_url)
                     url = url_list[0]
                     ydl_opts = {}
-                    os.chdir(path)
+                    # os.chdir(path)
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                 ydl.download([url])
 
         if flag == 0:
             sofiaResponse('I have not found anything in Youtube ')
+    elif len(command) != 0:
+        ErrorPrompt()
 
 
+tabFlag = False
+initalPrompt()
 while True:
     assistant(myCommand())
